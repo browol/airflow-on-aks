e4c68435dbe8bd4f1a67dac47433283579a04a91
browol <kittipol.mee@gmail.com>
2024-03-24 19:37:56 +0700

    update terraform azure vpn

---
+++ infrastructure/terraform/terraform-azure-vpn/generate-certificate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+CA_SUBJECT="${1:-/C=TH/CN=browol.io}"
+OUTPUT_DIR="certs"
+
+mkdir -p $OUTPUT_DIR
+cd $OUTPUT_DIR
+
+# Generate root ca key and certificate
+openssl genrsa -out root.key 4096
+openssl req -new -x509 -days 3650 -subj $CA_SUBJECT -key root.key -out root.crt
+
+# Generate client key, signing key request, and certificate
+openssl genrsa -out client.key 2048
+openssl req -new -subj $CA_SUBJECT -key client.key -out client.csr
+openssl x509 -req -days 365 -in client.csr -CA root.crt -CAkey root.key -CAcreateserial -out client.crt
+
+# Verify client certificate with root certificate
+openssl verify -CAfile root.crt client.crt
--- infrastructure/terraform/terraform-azure-vpn/main.tf
+++ infrastructure/terraform/terraform-azure-vpn/main.tf
@@ -1,48 +1,133 @@
 locals {
   client                    = "browol"
-  stack                     = "infra"
+  stack                     = "vpn"
   environment               = "dev"
   environment_short         = "d"
   location                  = "southeastasia"
   location_short            = "sa1"
-  vnet_addr_prefix          = "10.16.0.0/24"
-  subnet_addr_prefix        = "10.16.0.0/26"
+  aad_admin_group_object_id = "96c4ca86-584a-4584-9923-ac463c8b88af"
+  vnet_addr_prefix          = "10.0.0.0/16"
+  subnet_addr_prefix        = "10.0.1.0/24"
+  vpn_client_cidrs          = ["10.2.0.0/24"]
+  certificate_name          = "certs/root.crt"
   tags = {
     client      = local.client
     environment = local.environment
     stack       = local.stack
   }
+  naming_suffix = split(
+    "-",
+    format("%s-%s-%s-%s", local.environment_short, local.location_short, local.client, local.stack)
+  )
 }
 
-#####################
-## KeyVault - Main ##
-#####################
+module "region" {
+  source       = "claranet/regions/azurerm"
+  version      = "7.0.0"
+  azure_region = local.location
+}
 
-# Create the Azure Key Vault - globally unique - 24 characters max
-resource "azurerm_key_vault" "kopi-keyvault" {
-  name                = "${var.region}-${var.environment}-${var.app_name}-kv"
-  location            = azurerm_resource_group.kopi-rg.location
-  resource_group_name = azurerm_resource_group.kopi-rg.name
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.4.0"
+  suffix  = local.naming_suffix
+}
 
-  enabled_for_deployment          = var.kv-enabled-for-deployment
-  enabled_for_disk_encryption     = var.kv-enabled-for-disk-encryption
-  enabled_for_template_deployment = var.kv-enabled-for-template-deployment
-  tenant_id                       = var.azure-tenant-id
+data "azurerm_client_config" "current" {
+}
 
-  sku_name = var.kv-sku-name
+resource "azurerm_resource_group" "rg" {
+  name     = module.naming.resource_group.name
+  location = module.region.location
+  tags     = local.tags
+}
 
-  tags = {
-    environment = var.environment
-  }
+resource "azurerm_key_vault" "kv" {
+  name                = module.naming.key_vault.name_unique
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  tags                = local.tags
+
+  enabled_for_deployment          = true
+  enabled_for_disk_encryption     = true
+  enabled_for_template_deployment = true
+  tenant_id                       = data.azurerm_client_config.current.tenant_id
+
+  sku_name = "standard"
 
   access_policy {
-    tenant_id = var.azure-tenant-id
-    object_id = var.kv-full-object-id
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = local.aad_admin_group_object_id
 
-    certificate_permissions = var.kv-certificate-permissions-full
-    key_permissions         = var.kv-key-permissions-full
-    secret_permissions      = var.kv-secret-permissions-full
-    storage_permissions     = var.kv-storage-permissions-full
+    certificate_permissions = [
+      "Backup",
+      "Create",
+      "Delete",
+      "DeleteIssuers",
+      "Get",
+      "GetIssuers",
+      "Import",
+      "List",
+      "ListIssuers",
+      "ManageContacts",
+      "ManageIssuers",
+      "Purge",
+      "Recover",
+      "Restore",
+      "SetIssuers",
+      "Update"
+    ]
+
+    key_permissions = [
+      "Backup",
+      "Create",
+      "Decrypt",
+      "Delete",
+      "Encrypt",
+      "Get",
+      "Import",
+      "List",
+      "Purge",
+      "Recover",
+      "Restore",
+      "Sign",
+      "UnwrapKey",
+      "Update",
+      "Verify",
+      "WrapKey",
+      "Release",
+      "Rotate",
+      "GetRotationPolicy",
+      "SetRotationPolicy"
+    ]
+
+    secret_permissions = [
+      "Backup",
+      "Delete",
+      "Get",
+      "List",
+      "Purge",
+      "Recover",
+      "Restore",
+      "Set"
+    ]
+
+    storage_permissions = [
+      "Backup",
+      "Delete",
+      "DeleteSAS",
+      "Get",
+      "GetSAS",
+      "List",
+      "ListSAS",
+      "Purge",
+      "Recover",
+      "RegenerateKey",
+      "Restore",
+      "Set",
+      "SetSAS",
+      "Update"
+    ]
   }
 
   network_acls {
@@ -51,56 +136,34 @@ resource "azurerm_key_vault" "kopi-keyvault" {
   }
 }
 
-############################
-## KeyVault Secret - Main ##
-############################
-
-# Variable for Certificate Name
-locals {
-  certificate-name = "${var.company}-RootCert.crt"
-}
-
 # Create a Secret for the VPN Root certificate
-resource "azurerm_key_vault_secret" "vpn-root-certificate" {
-  depends_on=[azurerm_key_vault.kopi-keyvault]
+resource "azurerm_key_vault_secret" "vpn_root_certificate" {
+  name         = "vpn-p2s-root-certificate"
+  value        = filebase64(local.certificate_name)
+  key_vault_id = azurerm_key_vault.kv.id
+  tags         = local.tags
 
-  name         = "vpn-root-certificate"
-  value        = filebase64(local.certificate-name)
-  key_vault_id = azurerm_key_vault.kopi-keyvault.id
-
-  tags = {
-    environment = var.environment
-  }
-}
-
-########################
-## VPN Gateway - Main ##
-########################
-
-# Read Certificate
-data "azurerm_key_vault_secret" "vpn-root-certificate" {
-  depends_on=[
-    azurerm_key_vault.kopi-keyvault,
-    azurerm_key_vault_secret.vpn-root-certificate
+  depends_on = [
+    azurerm_key_vault.kv,
   ]
-
-  name         = "vpn-root-certificate"
-  key_vault_id = azurerm_key_vault.kopi-keyvault.id
 }
 
 # Create a Public IP for the Gateway
-resource "azurerm_public_ip" "kopi-gateway-ip" {
-  name                = "${var.region}-${var.environment}-${var.app_name}-gw-ip"
-  location            = azurerm_resource_group.kopi-rg.location
-  resource_group_name = azurerm_resource_group.kopi-rg.name
+resource "azurerm_public_ip" "gateway_pip" {
+  name                = module.naming.public_ip.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  tags                = local.tags
+
   allocation_method   = "Dynamic"
 }
 
 # Create VPN Gateway
-resource "azurerm_virtual_network_gateway" "kopi-vpn-gateway" {
-  name                = "${var.region}-${var.environment}-${var.app_name}-gw"
-  location            = azurerm_resource_group.kopi-rg.location
-  resource_group_name = azurerm_resource_group.kopi-rg.name
+resource "azurerm_virtual_network_gateway" "gateway" {
+  name                = module.naming.virtual_network_gateway.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  tags                = local.tags
 
   type     = "Vpn"
   vpn_type = "RouteBased"
@@ -110,51 +173,37 @@ resource "azurerm_virtual_network_gateway" "kopi-vpn-gateway" {
   sku           = "Basic"
 
   ip_configuration {
-    name                          = "${var.region}-${var.environment}-${var.app_name}-vnet"
-    public_ip_address_id          = azurerm_public_ip.kopi-gateway-ip.id
+    name                          = module.naming.virtual_network.name
+    public_ip_address_id          = azurerm_public_ip.gateway_pip.id
     private_ip_address_allocation = "Dynamic"
-    subnet_id                     = azurerm_subnet.kopi-gateway-subnet.id
+    subnet_id                     = azurerm_subnet.gateway.id
   }
 
   vpn_client_configuration {
-    address_space = ["10.2.0.0/24"]
+    address_space = local.vpn_client_cidrs
 
     root_certificate {
-      name = "VPNROOT"
-
-      public_cert_data = data.azurerm_key_vault_secret.vpn-root-certificate.value
+      name             = "vpn-p2s-root-certificate"
+      public_cert_data = azurerm_key_vault_secret.vpn_root_certificate.value
     }
-
-  }
-}
-####################
-## Network - Main ##
-####################
-
-# Create a Resource Group
-resource "azurerm_resource_group" "kopi-rg" {
-  name     = "${var.region}-${var.environment}-${var.app_name}-rg"
-  location = var.location
-  tags = {
-    environment = var.environment
   }
 }
 
 # Create the VNET
-resource "azurerm_virtual_network" "kopi-vnet" {
-  name                = "${var.region}-${var.environment}-${var.app_name}-vnet"
-  address_space       = [var.kopi-vnet-cidr]
-  location              = azurerm_resource_group.kopi-rg.location
-  resource_group_name   = azurerm_resource_group.kopi-rg.name
-  tags = {
-    environment = var.environment
-  }
+resource "azurerm_virtual_network" "vnet" {
+  name                = module.naming.virtual_network.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  tags                = local.tags
+
+  address_space = [local.vnet_addr_prefix]
 }
 
 # Create a Gateway Subnet
-resource "azurerm_subnet" "kopi-gateway-subnet" {
-  name                 = "GatewaySubnet" # do not rename
-  address_prefixes     = [var.kopi-gateway-subnet-cidr]
-  virtual_network_name = azurerm_virtual_network.kopi-vnet.name
-  resource_group_name  = azurerm_resource_group.kopi-rg.name
+resource "azurerm_subnet" "gateway" {
+  name                 = "GatewaySubnet" # do not change the name
+  resource_group_name  = azurerm_resource_group.rg.name
+  
+  address_prefixes     = [local.subnet_addr_prefix]
+  virtual_network_name = azurerm_virtual_network.vnet.name
 }
